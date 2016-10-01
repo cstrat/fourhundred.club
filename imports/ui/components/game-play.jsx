ada88a5377b7c605b1853d34fdf0127a6beea652
Chris Stratford <neester@gmail.com>
2016-10-01 10:09:32 +1000

    Refactoring coming along well, basic gameplay is working. Need to fix up the main scoreboard, status note, keypad note and tidy up the action-bar.

--- imports/ui/components/game-play.jsx
+++ imports/ui/components/game-play.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import { GameScoresComponent } from './game-scores';
 
 /*
   [Game Calls & Makes]
@@ -32,6 +32,9 @@ export class GamePlayComponent extends Component {
     this.keypadDisable        = this.keypadDisable.bind(this);
     this.keypadSelected       = this.keypadSelected.bind(this);
 
+    this.returnCallKeypad     = this.returnCallKeypad.bind(this);
+    this.returnMakeKeypad     = this.returnMakeKeypad.bind(this);
+
     this.saveCalls            = this.saveCalls.bind(this);
     this.saveCalls_return     = this.saveCalls_return.bind(this);
     this.saveCallsDisabled    = this.saveCallsDisabled.bind(this);
@@ -41,6 +44,7 @@ export class GamePlayComponent extends Component {
     this.saveMakesDisabled    = this.saveMakesDisabled.bind(this);
 
     this.nameClass            = this.nameClass.bind(this);
+    this.makeClass            = this.makeClass.bind(this);
     this.statusNote           = this.statusNote.bind(this);
 
     this.throwRound           = this.throwRound.bind(this);
@@ -79,12 +83,22 @@ export class GamePlayComponent extends Component {
     if (this.state.keypad_confirm == int) {
       // User has clicked the same number twice - save this in the state array
 
-      let calls = this.state.calls;
-      calls[this.state.keypad_player] = int;
+      var calls = this.state.calls;
+      var makes = this.state.makes;
+
+      switch (this.props.data.currentRound().status) {
+        case 0:
+          calls[this.state.keypad_player] = int;
+        break;
+        case 1:
+          makes[this.state.keypad_player] = int;
+        break;
+      }
 
       this.setState(
         {
           calls,
+          makes,
           keypad_player:    (this.state.keypad_player == 3) ? 0 : this.state.keypad_player + 1,
           keypad_confirm:   null
         }
@@ -201,13 +215,44 @@ export class GamePlayComponent extends Component {
     return (this.state.calls.includes(null)) ? true : false;
   }
 
-  nameClass(position) {
-    var className = (position == this.props.data.currentRound().dealer) ? 'dealer ' : '';
 
-    if (position == this.state.keypad_player) {
-      className += 'selected';
+
+  /*
+    [saveMakes]
+      Method call to save the round results to the database.
+  */
+  saveMakes(e) {
+    e.preventDefault();
+
+    Meteor.call(
+      'app.games.makes',
+      {
+        scoreCode:  FlowRouter.getParam('ScoreGameCode'),
+        makes:      this.state.makes
+      },
+      this.saveMakes_return
+    );
+  }
+
+
+  /*
+    [saveMakes_return]
+      Return function for the save makes method call.
+  */
+  saveMakes_return(error, result) {
+    this.setState({data_loading: false});
+
+    if (error) {
+      alert(error.reason);
+
+    }else if (result == 4) {
+      // This means the round was likely thrown. Need to reset the data
+      this.resetState();
     }
-    return className;
+  }
+
+  saveMakesDisabled() {
+    return (this.state.makes.includes(null) || this.state.makes.reduce(function(a, b) { return a + b; }) != 13) ? true : false;
   }
 
 
@@ -256,6 +301,23 @@ export class GamePlayComponent extends Component {
   }
 
 
+  nameClass(position) {
+    var className = (position == this.props.data.currentRound().dealer) ? 'dealer ' : '';
+
+    if (position == this.state.keypad_player) {
+      className += 'selected';
+    }
+    return className;
+  }
+
+  makeClass(position) {
+    var playerCall  = this.props.data.currentRound().calls[position];
+    var playerMake  = this.state.makes[position];
+    var className   = (playerMake !== null) ? (playerMake >= playerCall) ? 'made' : 'miss'  : '';
+
+    return className;
+  }
+
 
   /*
     [throwRound_selected]
@@ -269,7 +331,7 @@ export class GamePlayComponent extends Component {
 
   /*
     [statusNote]
-      Provide some humerous status notes to go with the number thats been called.
+      Provide status notes to go with the current status of the game.
   */
   statusNote() {
     if (this.state.throw_selected) {
@@ -310,22 +372,22 @@ export class GamePlayComponent extends Component {
           <li className={this.nameClass(0)}>
             <name>{this.props.data.players[0].name}</name>
             <score>{this.props.data.currentRound().scores[0]}</score>
-            <call>{this.state.calls[0] || '-'}</call>
+            <make className={this.makeClass(0)}>{this.props.data.currentRound().calls[0]} | {(this.state.makes[0] === null) ? '-' : this.state.makes[0]}</make>
           </li>
           <li className={this.nameClass(1)}>
             <name>{this.props.data.players[1].name}</name>
             <score>{this.props.data.currentRound().scores[1]}</score>
-            <call>{this.state.calls[1] || '-'}</call>
+            <make className={this.makeClass(1)}>{this.props.data.currentRound().calls[1]} | {(this.state.makes[1] === null) ? '-' : this.state.makes[1]}</make>
           </li>
           <li className={this.nameClass(2)}>
             <name>{this.props.data.players[2].name}</name>
             <score>{this.props.data.currentRound().scores[2]}</score>
-            <call>{this.state.calls[2] || '-'}</call>
+            <make className={this.makeClass(2)}>{this.props.data.currentRound().calls[2]} | {(this.state.makes[2] === null) ? '-' : this.state.makes[2]}</make>
           </li>
           <li className={this.nameClass(3)}>
             <name>{this.props.data.players[3].name}</name>
             <score>{this.props.data.currentRound().scores[3]}</score>
-            <call>{this.state.calls[3] || '-'}</call>
+            <make className={this.makeClass(3)}>{this.props.data.currentRound().calls[3]} | {(this.state.makes[3] === null) ? '-' : this.state.makes[3]}</make>
           </li>
         </ul>
 
@@ -335,42 +397,101 @@ export class GamePlayComponent extends Component {
           <div className="action-bar">
             <h2>{this.statusNote()}</h2>
 
-            { (this.saveCallsDisabled()) ? <button disabled={this.disableActionBar()} ref="throw" className={this.throwRound_selected()} onClick={this.throwRound}>Throw Round</button> : null }
-            <button disabled={this.disableActionBar()} ref="enter" onClick={this.keypadToggle}>{ (this.saveCallsDisabled()) ? 'Enter Calls →' : 'Re-enter Calls' } </button>
-            { (this.saveCallsDisabled()) ? null : <button disabled={this.disableActionBar()} ref="save" onClick={this.saveCalls}>Save Calls &rarr;</button> }
+            {(this.props.data.currentRound().status==0)?
+              <div>
+                { (this.saveCallsDisabled()) ? <button disabled={this.disableActionBar()} ref="throw" className={this.throwRound_selected()} onClick={this.throwRound}>Throw Round</button> : null }
+                <button disabled={this.disableActionBar()} ref="enter" onClick={this.keypadToggle}>{ (this.saveCallsDisabled()) ? 'Enter Calls →' : 'Re-enter Calls' } </button>
+                { (this.saveCallsDisabled()) ? null : <button disabled={this.disableActionBar()} ref="save" onClick={this.saveCalls}>Save Calls &rarr;</button> }
+              </div>
+            :
+              <div>
+                { (this.saveMakesDisabled()) ? <button disabled={this.disableActionBar()} ref="throw" className={this.throwRound_selected()} onClick={this.throwRound}>Throw Round</button> : null }
+                <button disabled={this.disableActionBar()} ref="enter" onClick={this.keypadToggle}>{ (this.saveMakesDisabled()) ? 'Enter Results →' : 'Re-enter Results' } </button>
+                { (this.saveMakesDisabled()) ? null : <button disabled={this.disableActionBar()} ref="save" onClick={this.saveMakes}>Save Results &rarr;</button> }
+              </div>
+            }
+
           </div>
         :
           <div>
-            <h2>Call for {this.props.data.players[this.state.keypad_player].name}. {this.state.keypad_confirm + this.state.calls.reduce(function(a, b) { return a + b; })}/{this.props.data.minimumCall()}</h2>
-            <ul className="keypad keypad-calls">
-              <li className="top-row">
-                <button disabled={this.keypadDisable(2)} className={this.keypadSelected(2)} onClick={()=>self.keypadEnter(2)}>2</button>
-                <button disabled={this.keypadDisable(3)} className={this.keypadSelected(3)} onClick={()=>self.keypadEnter(3)}>3</button>
-                <button className={this.keypadSelected(4)} onClick={()=>self.keypadEnter(4)}>4</button>
-              </li>
-              <li className="middle-row">
-                <button className={this.keypadSelected(5)} onClick={()=>self.keypadEnter(5)}>5</button>
-                <button className={this.keypadSelected(6)} onClick={()=>self.keypadEnter(6)}>6</button>
-                <button className={this.keypadSelected(7)} onClick={()=>self.keypadEnter(7)}>7</button>
-                <button className={this.keypadSelected(8)} onClick={()=>self.keypadEnter(8)}>8</button>
-              </li>
-              <li className="bottom-row">
-                <button className={this.keypadSelected(9)} onClick={()=>self.keypadEnter(9)}>9</button>
-                <button className={this.keypadSelected(10)} onClick={()=>self.keypadEnter(10)}>10</button>
-                <button className={this.keypadSelected(11)} onClick={()=>self.keypadEnter(11)}>11</button>
-                <button className={this.keypadSelected(12)} onClick={()=>self.keypadEnter(12)}>12</button>
-                <button className={this.keypadSelected(13)} onClick={()=>self.keypadEnter(13)}>13</button>
-              </li>
-              <li className="special-row">
-                <button onClick={this.keypadToggle}>Cancel Entry</button>
-              </li>
-            </ul>
+            {(this.props.data.currentRound().status==0)? this.returnCallKeypad() : this.returnMakeKeypad() }
           </div>
         }
 
         <hr />
 
+        <GameScoresComponent {...this.props}  />
+
       </div>
     );
   }
+
+
+  returnCallKeypad() {
+
+    var self = this;
+
+    return(
+      <ul className="keypad keypad-calls">
+        <li className="top-row">
+          <button disabled={this.keypadDisable(2)} className={this.keypadSelected(2)} onClick={()=>self.keypadEnter(2)}>2</button>
+          <button disabled={this.keypadDisable(3)} className={this.keypadSelected(3)} onClick={()=>self.keypadEnter(3)}>3</button>
+          <button className={this.keypadSelected(4)} onClick={()=>self.keypadEnter(4)}>4</button>
+        </li>
+        <li className="middle-row">
+          <button className={this.keypadSelected(5)} onClick={()=>self.keypadEnter(5)}>5</button>
+          <button className={this.keypadSelected(6)} onClick={()=>self.keypadEnter(6)}>6</button>
+          <button className={this.keypadSelected(7)} onClick={()=>self.keypadEnter(7)}>7</button>
+          <button className={this.keypadSelected(8)} onClick={()=>self.keypadEnter(8)}>8</button>
+        </li>
+        <li className="bottom-row">
+          <button className={this.keypadSelected(9)} onClick={()=>self.keypadEnter(9)}>9</button>
+          <button className={this.keypadSelected(10)} onClick={()=>self.keypadEnter(10)}>10</button>
+          <button className={this.keypadSelected(11)} onClick={()=>self.keypadEnter(11)}>11</button>
+          <button className={this.keypadSelected(12)} onClick={()=>self.keypadEnter(12)}>12</button>
+          <button className={this.keypadSelected(13)} onClick={()=>self.keypadEnter(13)}>13</button>
+        </li>
+        <li className="special-row">
+          <button onClick={this.keypadToggle}>Cancel Entry</button>
+        </li>
+      </ul>
+    );
+  }
+
+
+  returnMakeKeypad() {
+
+    var self = this;
+
+    return(
+      <ul className="keypad keypad-makes">
+        <li className="top-row">
+          <button className={this.keypadSelected(0)} onClick={()=>self.keypadEnter(0)}>0</button>
+          <button disabled={this.keypadDisable(1)} className={this.keypadSelected(1)} onClick={()=>self.keypadEnter(1)}>1</button>
+          <button disabled={this.keypadDisable(2)} className={this.keypadSelected(2)} onClick={()=>self.keypadEnter(2)}>2</button>
+          <button disabled={this.keypadDisable(3)} className={this.keypadSelected(3)} onClick={()=>self.keypadEnter(3)}>3</button>
+        </li>
+        <li className="middle-row">
+          <button disabled={this.keypadDisable(4)} className={this.keypadSelected(4)} onClick={()=>self.keypadEnter(4)}>4</button>
+          <button disabled={this.keypadDisable(5)} className={this.keypadSelected(5)} onClick={()=>self.keypadEnter(5)}>5</button>
+          <button disabled={this.keypadDisable(6)} className={this.keypadSelected(6)} onClick={()=>self.keypadEnter(6)}>6</button>
+          <button disabled={this.keypadDisable(7)} className={this.keypadSelected(7)} onClick={()=>self.keypadEnter(7)}>7</button>
+          <button disabled={this.keypadDisable(8)} className={this.keypadSelected(8)} onClick={()=>self.keypadEnter(8)}>8</button>
+        </li>
+        <li className="bottom-row">
+          <button disabled={this.keypadDisable(9)} className={this.keypadSelected(9)} onClick={()=>self.keypadEnter(9)}>9</button>
+          <button disabled={this.keypadDisable(10)} className={this.keypadSelected(10)} onClick={()=>self.keypadEnter(10)}>10</button>
+          <button disabled={this.keypadDisable(11)} className={this.keypadSelected(11)} onClick={()=>self.keypadEnter(11)}>11</button>
+          <button disabled={this.keypadDisable(12)} className={this.keypadSelected(12)} onClick={()=>self.keypadEnter(12)}>12</button>
+          <button disabled={this.keypadDisable(13)} className={this.keypadSelected(13)} onClick={()=>self.keypadEnter(13)}>13</button>
+        </li>
+        <li className="special-row">
+          <button onClick={this.keypadToggle}>Cancel Entry</button>
+        </li>
+      </ul>
+    );
+  }
+
+
+
 }
